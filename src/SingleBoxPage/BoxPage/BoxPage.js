34882887bc82bb93136393e8cb26815d777e241d
Zoe <zoethomas1540@gmail.com>
2022-05-16 21:18:22 +0100

    Send credentials with order request

--- src/SingleBoxPage/BoxPage/BoxPage.js
+++ src/SingleBoxPage/BoxPage/BoxPage.js
@@ -13,6 +13,22 @@ class BoxPage extends Component {
     contentsLoaded: false
   }
 
+  getCookie = (cname) => {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
   async componentDidMount() {
     axios.get('https://dry-forest-94057.herokuapp.com/boxes/' + this.props.id, {
       headers: {
