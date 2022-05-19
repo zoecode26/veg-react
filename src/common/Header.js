4c91f1398d72eb7c32b6b8bffded5773cfe6fb5b
Zoe <zoethomas1540@gmail.com>
2022-05-19 09:17:44 +0100

    Send credentials with order request

--- src/common/Header.js
+++ src/common/Header.js
@@ -6,12 +6,10 @@ import {
 } from "@material-ui/core";
 import styles from './Header.module.css';
 import { Link } from "react-router-dom";
-import { CssBaseline } from "@mui/material";
 
 export default function Header() {
     return (
         <AppBar position="static">
-            {/* <CssBaseline /> */}
             <Toolbar className={styles.toolbar}>
                 <Typography variant="h4">
                     <Link to={"/"} style={{ textDecoration: 'none', color: 'white' }}>
--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -2,41 +2,44 @@ import React, { Component } from "react";
 import {
   AppBar,
   Toolbar,
-  CssBaseline,
-  Typography,
+  CssBaseline
 } from "@material-ui/core";
 import styles from './Navbar.module.css';
 import { Link } from "react-router-dom";
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Baseline from "./Baseline";
+import getCookie from './Utils'
 
 class Navbar extends Component {
   state = {
     signedIn: false,
+    authPath: "/login",
+    authText: "Login/Signup",
     loaded: false,
   }
 
-  getCookie = (cname) => {
-    let name = cname + "=";
-    let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
-    for(let i = 0; i <ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
+  // getCookie = (cname) => {
+  //   let name = cname + "=";
+  //   let decodedCookie = decodeURIComponent(document.cookie);
+  //   let ca = decodedCookie.split(';');
+  //   for(let i = 0; i <ca.length; i++) {
+  //     let c = ca[i];
+  //     while (c.charAt(0) == ' ') {
+  //       c = c.substring(1);
+  //     }
+  //     if (c.indexOf(name) == 0) {
+  //       return c.substring(name.length, c.length);
+  //     }
+  //   }
+  //   return "";
+  // }
 
   async componentDidMount() {
-    let email = this.getCookie("email");
+    let email = getCookie("email");
     if (email != "") {
       this.setState({
         signedIn: true, 
+        authPath: "/signout",
+        authText: "Signout"
       });
     }
     this.setState({
@@ -46,54 +49,30 @@ class Navbar extends Component {
 
   render() {
     if (this.state.loaded) {
-      if (this.state.signedIn) {
-        return (
-          <AppBar position="static">
-            <CssBaseline />
-            <Toolbar className={styles.toolbar}>
-              <div className={styles.navlinks}>
-                <Link to="/" className={styles.link}>
-                  Boxes
-                </Link>
-                <Link to="/orders" className={styles.link}>
-                  My Orders
-                </Link>
-                <Link to="/signout" className={styles.link}>
-                  Sign Out
-                </Link>
-                <Link to="/cart" className={styles.link}>
-                  <ShoppingCartIcon fontSize="medium"/>
-                </Link>
-              </div>
-            </Toolbar>
-          </AppBar>
-        );
-      } else {
-        return (
-          <AppBar position="static">
-            <CssBaseline />
-            <Toolbar className={styles.toolbar}>
-              <div className={styles.navlinks}>
-                <Link to="/" className={styles.link}>
-                  Boxes
-                </Link>
-                <Link to="orders" className={styles.link}>
-                  My Orders
-                </Link>
-                <Link to="/login" className={styles.link}>
-                  Login/Signup
-                </Link>
-                <Link to="/cart" className={styles.link}>
-                  <ShoppingCartIcon fontSize="medium"/>
-                </Link>
-              </div>
-            </Toolbar>
-          </AppBar>
-        );
-      }
-    }
+      return (
+        <AppBar position="static">
+          <CssBaseline />
+          <Toolbar className={styles.toolbar}>
+            <div className={styles.navlinks}>
+              <Link to="/" className={styles.link}>
+                Boxes
+              </Link>
+              <Link to="/orders" className={styles.link}>
+                My Orders
+              </Link>
+              <Link to={this.state.authPath} className={styles.link}>
+                {this.state.authText}
+              </Link>
+              <Link to="/cart" className={styles.link}>
+                <ShoppingCartIcon fontSize="medium"/>
+              </Link>
+            </div>
+          </Toolbar>
+        </AppBar>
+      );
+    } 
     return null;
-  }
+  }  
 }
 
 export default Navbar;
