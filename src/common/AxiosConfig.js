2a342e8e58688d393812ba924fea690038886787
Zoe <zoethomas1540@gmail.com>
2022-05-20 08:30:24 +0100

    Refactoring

--- src/common/AxiosConfig.js
+++ src/common/AxiosConfig.js
@@ -1,4 +1,5 @@
 import getCookie from './Utils'
+import axios from 'axios';
 
 const instance = axios.create();
 instance.defaults.headers.common['Authorization'] = "Bearer " + getCookie("jwt-token");
